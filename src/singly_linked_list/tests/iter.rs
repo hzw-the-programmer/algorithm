240f2ed7670d2df69d1ac2fd8cd9e4669228d188
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-09 17:35:56 +0800

    add singly linked list iter/iter_mut test cases

--- src/singly_linked_list/tests/iter.rs
+++ src/singly_linked_list/tests/iter.rs
@@ -21,3 +21,16 @@ fn test_2() {
     }
     assert_eq!(v, vec![&2, &1, &3]);
 }
+
+#[test]
+fn iter() {
+    let mut list = SinglyLinkedList::new();
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), None);
+}
--- src/singly_linked_list/tests/iter_mut.rs
+++ src/singly_linked_list/tests/iter_mut.rs
@@ -11,3 +11,16 @@ fn test() {
     }
     assert_eq!(l.iter().collect::<Vec<&i32>>(), vec![&2, &3, &4]);
 }
+
+#[test]
+fn iter_mut() {
+    let mut list = SinglyLinkedList::new();
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+    let mut iter = list.iter_mut();
+    assert_eq!(iter.next(), Some(&mut 3));
+    assert_eq!(iter.next(), Some(&mut 2));
+    assert_eq!(iter.next(), Some(&mut 1));
+    assert_eq!(iter.next(), None);
+}
