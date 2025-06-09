a600c71fa9d856cc7212edd3c08b1e8bdc1d9785
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-09 17:49:58 +0800

    refactor singly linked list

--- src/singly_linked_list/v1/basic.rs
+++ src/singly_linked_list/v1/basic.rs
@@ -23,13 +23,15 @@ impl<T> Node<T> {
     }
 }
 
-pub struct SinglyLinkedList<T> {
+pub type SinglyLinkedList<T> = List<T>;
+
+pub struct List<T> {
     pub(crate) head: Link<T>,
     pub(super) tail: *mut Node<T>,
     pub(super) len: usize,
 }
 
-impl<T> SinglyLinkedList<T> {
+impl<T> List<T> {
     pub fn new() -> Self {
         Self {
             head: None,
@@ -59,14 +61,6 @@ impl<T> SinglyLinkedList<T> {
         self.len += 1;
     }
 
-    pub fn peek_front(&self) -> Option<&T> {
-        self.head.as_deref().map(|node| &node.value)
-    }
-
-    pub fn peek_front_mut(&mut self) -> Option<&mut T> {
-        self.head.as_deref_mut().map(|node| &mut node.value)
-    }
-
     pub fn pop_front(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
@@ -90,5 +84,15 @@ impl<T> SinglyLinkedList<T> {
 
     pub fn is_empty(&self) -> bool {
         self.len == 0
+    }
+}
+
+impl<T> List<T> {
+    pub fn peek_front(&self) -> Option<&T> {
+        self.head.as_deref().map(|node| &node.value)
+    }
+
+    pub fn peek_front_mut(&mut self) -> Option<&mut T> {
+        self.head.as_deref_mut().map(|node| &mut node.value)
     }
 }
