4c7247bc121a042ec171431316025b442ef72788
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-12 11:51:04 +0800

    singly linked list get_mut

--- src/singly_linked_list/raw/index.rs
+++ src/singly_linked_list/raw/index.rs
@@ -1,10 +1,16 @@
 use super::*;
-use core::ops::Index;
+use core::ops::{Index, IndexMut};
 
 impl<T> Index<usize> for List<T> {
     type Output = T;
 
     fn index(&self, idx: usize) -> &Self::Output {
         self.get(idx).unwrap()
+    }
+}
+
+impl<T> IndexMut<usize> for List<T> {
+    fn index_mut(&mut self, idx: usize) -> &mut Self::Output {
+        self.get_mut(idx).unwrap()
     }
 }
--- src/singly_linked_list/tests/index.rs
+++ src/singly_linked_list/tests/index.rs
@@ -23,3 +23,30 @@ fn index_2() {
     assert_eq!(list[2], 3);
     assert_eq!(list[3], 4);
 }
+
+#[test]
+fn index_mut() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list[0] = 2;
+    list[1] = 3;
+    list[2] = 4;
+    assert_eq!(list[0], 2);
+    assert_eq!(list[1], 3);
+    assert_eq!(list[2], 4);
+}
+
+#[test]
+#[should_panic]
+fn index_mut_2() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list[0] = 2;
+    list[1] = 3;
+    list[2] = 4;
+    list[3] = 5;
+}
